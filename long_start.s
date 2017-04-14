790fc7df59de11fa42e6a8744ad6d7fdd9c06bfd
Cary Haynie <cary.haynie@gmail.com>
2017-04-14 19:56:29 -0400

    update long_start.s to gas syntax.

--- long_start.s
+++ long_start.s
@@ -1,16 +1,16 @@
-section .text
-bits 64
+.text
+.code64
 
-global long_start
+.global long_start
 long_start:
-    extern rust_main
+    .extern rust_main
     call rust_main
 .os_returned:
-    ; rust main returned, print `OS returned!`
-    mov rax, 0x4f724f204f534f4f
-    mov [0xb8000], rax
-    mov rax, 0x4f724f754f744f65
-    mov [0xb8008], rax
-    mov rax, 0x4f214f644f654f6e
-    mov [0xb8010], rax
+    /* rust main returned, print `OS returned!` */
+    movq $0x4f724f204f534f4f, %rax
+    movq %rax, ($0xb8000)
+    movq $0x4f724f754f744f65, %rax
+    movq %rax, ($0xb8008)
+    movq $0x4f214f644f654f6e, %rax
+    movq %rax, ($0xb8010)
     hlt
